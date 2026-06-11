7397bebe41d973383e7c44a86476960caf39c102
soniceggman419-ship-it <soniceggman419@gmail.com>
2026-06-11 15:23:43 -0700

    Update pacstrap command to exclude 'iptils'
    Removed 'iptils' from the pacstrap command.

--- arch1.sh
+++ arch1.sh
@@ -6,5 +6,5 @@ mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 swapon /dev/sda2
 reflector --latest 5 --country US --protocol http,https rate --save /etc/pacman.d/mirrorlist
-pacstrap -K /mnt base linux linux-firmware pacman base-devel networkmanager curl vim iptils reflector vi grub efibootmgr
+pacstrap -K /mnt base linux linux-firmware pacman base-devel networkmanager curl vim reflector vi grub efibootmgr
 genfstab -U /mnt >> /mnt/etc/fstab
