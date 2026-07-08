3e909588f12d8ccf8e7adb64a5254767d3f3cdb2
jeff <soniceggman419@gmail.com>
2026-07-07 18:32:49 -0700

    editor arch2.sh remove try to add password tousers

--- arch2.sh
+++ arch2.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/bash
-echo this script will setup arch linux 
+echo this script will setup arch linux
 useradd -m -G wheel,users elijah
 echo the user name elijah wast just make on you system pending operation press crtl+c to stop this NOW
 sleep 3
-echo Enter the password for the root user
-passwd
 sleep 1
-echo Enter the password for you user
-passwd elijah
 reflector --latest 5 --country US --protocol http,https rate --save /etc/pacman.d/mirrorlist
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
